f2dbe21c87cc8ab7883a209a79180c5a4527d315
unknown <bmulaw@bu.edu>
2021-12-31 23:58:02 -0800

    app displays added list

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.util.Log
+//import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            // code here will be executed when btn is clicked
+//            Log.i("messageHere", "hello world")
+//        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = TaskItemAdapter(listOfTasks)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
